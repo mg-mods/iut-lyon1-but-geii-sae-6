e6cde1f161d5386c1b3553b3188ecd02f3b43346
Goupilou <Goupil@mailo.com>
2026-01-23 16:56:32 +0100

    Version bien de l'interface + sous-menu
    Removed houseLock.h and added images.h to manage image resources. Updated main.cpp to use the new images header. Replaced house-lock PNG with new SVG assets (exit, gear, restart, siren) and updated IHM_Hub.odg accordingly.

--- Code/hub/Hub/src/main.cpp
+++ Code/hub/Hub/src/main.cpp
@@ -1,74 +1,160 @@
 #include <M5Core2.h>
-#include "houseLock.h"
+#include "images.h"
 
-#define COLOR_BG TFT_BLACK
-#define COLOR_BTN_RED 0xE8E4
-#define COLOR_BTN_GREEN 0x07E0
-#define COLOR_BTN_BLUE 0x039F
-#define COLOR_BTN_GREY 0x7BEF
-#define COLOR_TEXT_WHITE TFT_WHITE
-#define COLOR_TEXT_ACCENT 0xFA48
+constexpr uint16_t C_BG = TFT_BLACK;
+constexpr uint16_t C_RED = 0xE8E4, C_GREEN = 0x07E0, C_DARKGREEN = 0x0546, C_BLUE = 0x039F, C_GREY = 0x7BEF;
+constexpr uint16_t C_WHITE = TFT_WHITE, C_RED1 = 0xFEBA, C_RED2 = 0xFD34, C_RED3 = 0xFB6D, C_RED4 = 0xF9C7, C_RED5 = 0xF800;
+constexpr uint16_t C_OPT = 0x18E3; // Gris foncé pour les options
 
-// Dimensions de l'image déduites de la taille du tableau (5000 éléments)
-// Le formatage du fichier .h suggère une largeur de 50 pixels
-#define LOCK_IMG_WIDTH 50
-#define LOCK_IMG_HEIGHT 50
+constexpr int W = 320, H = 240;
+constexpr int HEADER_H = 50, MGN = 10, GAP = 10;
+constexpr int BTN_W = (W - (MGN * 2) - GAP) / 2;
 
-Button btnLock(10, 90, 145, 80, false, "Verrouiller");
-Button btnEmptyGreen(165, 90, 145, 80, false, "VideVert");
-Button btnEmptyBlue(10, 180, 145, 50, false, "VideBleu");
-Button btnEmptyGrey(165, 180, 145, 50, false, "VideGris");
+constexpr int R1_Y = 90, R1_H = 80, R1_R = 15;
+constexpr int R2_Y = 180, R2_H = 50, R2_R = 10;
+constexpr int C1_X = MGN, C2_X = MGN + BTN_W + GAP;
 
-void drawInterface() {
-    M5.Lcd.fillScreen(COLOR_BG);
+// --- Grille Options (3 colonnes x 2 lignes) ---
+// Largeur bouton option: (320 - 20 marge - 20 gap) / 3 = 93px
+constexpr int O_W = 93, O_H = 50, O_R = 8;
+constexpr int O_Y1 = 60, O_Y2 = 120;
+constexpr int O_X1 = 10, O_X2 = 113, O_X3 = 216;
 
-    // Barre de titre
-    M5.Lcd.fillRect(0, 0, 320, 50, COLOR_BTN_GREEN);
+enum Screen {
+    HOME,
+    OPTIONS
+};
+
+Screen currentScreen = HOME;
+bool locked = false;
+
+// Boutons Accueil
+Button bLock(C1_X, R1_Y, BTN_W, R1_H, false, "L");
+Button bGreen(C2_X, R1_Y, BTN_W, R1_H, false, "G");
+Button bBlue(C1_X, R2_Y, BTN_W, R2_H, false, "B");
+Button bGrey(C2_X, R2_Y, BTN_W, R2_H, false, "GR");
+
+// Boutons Options
+Button bOpt1(O_X1, O_Y1, O_W, O_H, false, "O1");
+Button bOpt2(O_X2, O_Y1, O_W, O_H, false, "O2");
+Button bOpt3(O_X3, O_Y1, O_W, O_H, false, "O3");
+Button bOpt4(O_X1, O_Y2, O_W, O_H, false, "O4");
+Button bOpt5(O_X2, O_Y2, O_W, O_H, false, "O5");
+Button bOpt6(O_X3, O_Y2, O_W, O_H, false, "O6");
+
+void animateBtn(int x, int y, int w, int h, int r, uint16_t c) {
+    M5.Lcd.drawRoundRect(x, y, w, h, r, C_WHITE);
+    delay(100);
+    M5.Lcd.drawRoundRect(x, y, w, h, r, c);
+}
+
+void drawHome() {
+    uint16_t hBg = locked ? C_RED : C_DARKGREEN;
+
+    M5.Lcd.fillScreen(C_BG);
+    M5.Lcd.fillRect(0, 0, W, HEADER_H, hBg);
     
     M5.Lcd.setTextSize(1);
     M5.Lcd.setFreeFont(&FreeSansBold18pt7b);
     M5.Lcd.setTextDatum(ML_DATUM);
     
-    String text1 = "Alarme Connectee";
-    String text2 = "+";
+    String t1 = "Alarme Conne", t2 = "c", t3 = "t", t4 = "e", t5 = "e", t6 = "+";
+    int startX = 1;
     
-    int totalWidth = M5.Lcd.textWidth(text1) + M5.Lcd.textWidth(text2);
-    int startX = (320 - totalWidth) / 2;
-    
-    M5.Lcd.setTextColor(COLOR_TEXT_WHITE, COLOR_BTN_GREEN);
-    M5.Lcd.drawString(text1, startX, 28);
-    
-    M5.Lcd.setTextColor(COLOR_TEXT_ACCENT, COLOR_BTN_GREEN);
-    M5.Lcd.drawString(text2, startX + M5.Lcd.textWidth(text1), 28);
+    if (locked) {
+        M5.Lcd.setTextColor(C_WHITE, hBg);
+        M5.Lcd.drawString("Alarme Declenchee", startX, 24);
+    } else {
+        M5.Lcd.setTextColor(C_WHITE, hBg);
+        M5.Lcd.drawString(t1, startX, 24);
+        M5.Lcd.setTextColor(C_RED1, hBg);
+        M5.Lcd.drawString(t2, startX + M5.Lcd.textWidth(t1), 24);
+        M5.Lcd.setTextColor(C_RED2, hBg);
+        M5.Lcd.drawString(t3, startX + M5.Lcd.textWidth(t1) + M5.Lcd.textWidth(t2), 24);
+        M5.Lcd.setTextColor(C_RED3, hBg);
+        M5.Lcd.drawString(t4, startX + M5.Lcd.textWidth(t1) + M5.Lcd.textWidth(t2) + M5.Lcd.textWidth(t3), 24);
+        M5.Lcd.setTextColor(C_RED4, hBg);
+        M5.Lcd.drawString(t5, startX + M5.Lcd.textWidth(t1) + M5.Lcd.textWidth(t2) + M5.Lcd.textWidth(t3) + M5.Lcd.textWidth(t4), 24);
+        M5.Lcd.setTextColor(C_RED5, hBg);
+        M5.Lcd.drawString(t6, startX + M5.Lcd.textWidth(t1) + M5.Lcd.textWidth(t2) + M5.Lcd.textWidth(t3) + M5.Lcd.textWidth(t4) + M5.Lcd.textWidth(t5), 22);
+    }
 
     M5.Lcd.setFreeFont(&FreeSans12pt7b);
-    M5.Lcd.setTextColor(COLOR_TEXT_WHITE, COLOR_BG);
+    M5.Lcd.setTextColor(C_WHITE, C_BG);
     M5.Lcd.setTextDatum(TC_DATUM);
-    M5.Lcd.drawString("Vous etes protege", 160, 60);
+    M5.Lcd.drawString(locked ? "Alarme en cours" : "Vous etes protege", W / 2, 60);
 
-    // --- Bouton Verrouiller (Rouge) ---
-    M5.Lcd.fillRoundRect(10, 90, 145, 80, 15, COLOR_BTN_RED);
-    
-    // Calcul pour centrer l'image dans le bouton
-    // Bouton: x=10, y=90, w=145, h=80
-    int imgX = 10 + (145 - LOCK_IMG_WIDTH) / 2;
-    int imgY = 78 + (80 - LOCK_IMG_HEIGHT) / 2;
+    M5.Lcd.fillRoundRect(C1_X, R1_Y, BTN_W, R1_H, R1_R, C_RED);
+    M5.Lcd.pushImage(C1_X + (BTN_W - 50) / 2, R1_Y + (R1_H - 50) / 2 - 12, 50, 50, (uint16_t *)siren, 0x0000);
 
-    // Affichage de l'image avec gestion de la transparence
-    // 0x0000 (Noir) est défini comme couleur transparente
-    M5.Lcd.pushImage(imgX, imgY, LOCK_IMG_WIDTH, LOCK_IMG_HEIGHT, (uint16_t *)house_lock, 0x0000);
-
-    M5.Lcd.setFreeFont(&FreeSans12pt7b);
     M5.Lcd.setTextDatum(BC_DATUM);
-    // Le texte s'affichera par-dessus l'image si nécessaire
-    // Définition de la couleur du texte (Blanc) ET du fond (Rouge bouton)
-    M5.Lcd.setTextColor(COLOR_TEXT_WHITE, COLOR_BTN_RED);
-    M5.Lcd.drawString("Verrouiller", 82, 165);
+    M5.Lcd.setTextColor(C_WHITE, C_RED);
+    M5.Lcd.drawString("Alarme", C1_X + BTN_W / 2, 165);
 
-    // --- Autres Boutons ---
-    M5.Lcd.fillRoundRect(165, 90, 145, 80, 15, COLOR_BTN_GREEN);
-    M5.Lcd.fillRoundRect(10, 180, 145, 50, 10, COLOR_BTN_BLUE);
-    M5.Lcd.fillRoundRect(165, 180, 145, 50, 10, COLOR_BTN_GREY);
+    M5.Lcd.fillRoundRect(C2_X, R1_Y, BTN_W, R1_H, R1_R, C_GREEN);
+    M5.Lcd.setTextColor(C_WHITE, C_GREEN);
+    M5.Lcd.setTextDatum(BR_DATUM);
+    M5.Lcd.drawString("Deverouiller", C2_X + BTN_W - 13, 165);
+
+    M5.Lcd.fillRoundRect(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_BLUE);
+    M5.Lcd.setTextColor(C_WHITE, C_BLUE);
+    M5.Lcd.setTextDatum(BC_DATUM);
+    M5.Lcd.drawString("Options", (C1_X + BTN_W / 2) + 25, 218);
+    // Note: Assure-toi que 'gear' est bien dans images.h, sinon commente la ligne suivante
+    M5.Lcd.pushImage((C1_X + (BTN_W - 50) / 2) - 43, R2_Y + (R2_H - 50) / 2, 50, 50, (uint16_t *)gear, 0x0000);
+    
+    M5.Lcd.fillRoundRect(C2_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
+    M5.Lcd.setTextColor(C_WHITE, C_GREY);
+    M5.Lcd.setTextDatum(BC_DATUM);
+    M5.Lcd.drawString("Redemarrer", (C2_X + BTN_W / 2) + 25, 218);
+    // Note: Assure-toi que 'reboot_image' est bien dans images.h, sinon commente la ligne suivante
+    M5.Lcd.pushImage((C2_X + (BTN_W - 50) / 2) - 43, R2_Y + (R2_H - 50) / 2, 50, 50, (uint16_t *)reboot_image, 0x0000);
+}
+
+void drawOptions() {
+    M5.Lcd.fillScreen(C_BG);
+    M5.Lcd.fillRect(0, 0, W, HEADER_H, C_BLUE);
+
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setFreeFont(&FreeSansBold18pt7b);
+    M5.Lcd.setTextDatum(ML_DATUM);
+    M5.Lcd.setTextColor(C_WHITE, C_BLUE);
+    M5.Lcd.drawString("Reglages", 10, 28);
+
+    // Style de texte pour les boutons
+    M5.Lcd.setFreeFont(&FreeSans12pt7b);
+    M5.Lcd.setTextDatum(MC_DATUM);
+    M5.Lcd.setTextColor(C_WHITE, C_BLUE);
+
+    // Ligne 1
+    M5.Lcd.fillRoundRect(C1_X, R1_Y, BTN_W, R1_H, R1_R, C_BLUE);
+    M5.Lcd.drawString("Wifi", O_X1 + BTN_W/2, O_Y1 + O_H/2);
+
+    M5.Lcd.fillRoundRect(C2_X, R1_Y, BTN_W, R1_H, R1_R, C_BLUE);
+    M5.Lcd.drawString("Son", O_X2 + BTN_W/2, O_Y1 + O_H/2);
+
+    M5.Lcd.fillRoundRect(O_X3, O_Y1, BTN_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Ecran", O_X3 + BTN_W/2, O_Y1 + O_H/2);
+
+    // Ligne 2
+    M5.Lcd.fillRoundRect(O_X1, O_Y2, BTN_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Journal", O_X1 + BTN_W/2, O_Y2 + O_H/2);
+
+    M5.Lcd.fillRoundRect(O_X2, O_Y2, BTN_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Capteur", O_X2 + BTN_W/2, O_Y2 + O_H/2);
+
+    // Bouton Retour
+    M5.Lcd.fillRoundRect(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
+    M5.Lcd.setTextColor(C_WHITE, C_GREY);
+    M5.Lcd.drawString("Retour", C1_X + BTN_W / 2, R2_Y + R2_H / 2);
+}
+
+void drawInterface() {
+    if (currentScreen == HOME) {
+        drawHome();
+    } else {
+        drawOptions();
+    }
 }
 
 void setup() {
@@ -79,23 +165,37 @@ void setup() {
 void loop() {
     M5.update();
     
-    if (btnLock.wasPressed()) {
-        Serial.println("Action: Verrouiller");
-        // Animation simple du bouton
-        M5.Lcd.drawRoundRect(10, 90, 145, 80, 15, TFT_WHITE);
-        delay(100);
-        M5.Lcd.drawRoundRect(10, 90, 145, 80, 15, COLOR_BTN_RED);
-    }
-    
-    if (btnEmptyGreen.wasPressed()) {
-        Serial.println("Action: Zone Verte");
-    }
-    
-    if (btnEmptyBlue.wasPressed()) {
-        Serial.println("Action: Zone Bleue");
-    }
+    if (currentScreen == HOME) {
+        if (bLock.wasPressed()) { 
+            if (!locked) { locked = true; drawInterface(); }
+            animateBtn(C1_X, R1_Y, BTN_W, R1_H, R1_R, C_RED);
+        }
+        if (bGreen.wasPressed()) {
+            if (locked) { locked = false; drawInterface(); }
+            animateBtn(C2_X, R1_Y, BTN_W, R1_H, R1_R, C_GREEN);
+        }
+        if (bBlue.wasPressed()) {
+            animateBtn(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_BLUE);
+            currentScreen = OPTIONS;
+            drawInterface();
+        }
+        if (bGrey.wasPressed()) {
+             animateBtn(C2_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
+        }
 
-    if (btnEmptyGrey.wasPressed()) {
-        Serial.println("Action: Zone Grise");
+    } else if (currentScreen == OPTIONS) {
+        // Bouton Retour
+        if (bBlue.wasPressed()) {
+            animateBtn(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
+            currentScreen = HOME;
+            drawInterface();
+        }
+        // Gestion des 6 boutons (Animation simple pour l'instant)
+        if (bOpt1.wasPressed()) animateBtn(O_X1, O_Y1, O_W, O_H, O_R, C_OPT);
+        if (bOpt2.wasPressed()) animateBtn(O_X2, O_Y1, O_W, O_H, O_R, C_OPT);
+        if (bOpt3.wasPressed()) animateBtn(O_X3, O_Y1, O_W, O_H, O_R, C_OPT);
+        if (bOpt4.wasPressed()) animateBtn(O_X1, O_Y2, O_W, O_H, O_R, C_OPT);
+        if (bOpt5.wasPressed()) animateBtn(O_X2, O_Y2, O_W, O_H, O_R, C_OPT);
+        if (bOpt6.wasPressed()) animateBtn(O_X3, O_Y2, O_W, O_H, O_R, C_OPT);
     }
 }

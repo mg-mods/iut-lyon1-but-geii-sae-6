af7fb01c8e0bfc75da140dfeec2ae4419afca9c9
Goupilou <Goupil@mailo.com>
2026-01-27 11:04:00 +0100

    Amélioration globale de l'interface
    Buttons are now managed via pointers with dynamic allocation and cleanup to improve memory handling. UI layout for options screen is updated for better consistency, and button initialization is separated for each screen. Also adds updated IHM_Hub.odg and new IHM_Hub.pdf files.

--- Code/hub/Hub/src/main.cpp
+++ Code/hub/Hub/src/main.cpp
@@ -4,7 +4,7 @@
 constexpr uint16_t C_BG = TFT_BLACK;
 constexpr uint16_t C_RED = 0xE8E4, C_GREEN = 0x07E0, C_DARKGREEN = 0x0546, C_BLUE = 0x039F, C_GREY = 0x7BEF;
 constexpr uint16_t C_WHITE = TFT_WHITE, C_RED1 = 0xFEBA, C_RED2 = 0xFD34, C_RED3 = 0xFB6D, C_RED4 = 0xF9C7, C_RED5 = 0xF800;
-constexpr uint16_t C_OPT = 0x18E3; // Gris foncé pour les options
+constexpr uint16_t C_OPT = 0x18E3; 
 
 constexpr int W = 320, H = 240;
 constexpr int HEADER_H = 50, MGN = 10, GAP = 10;
@@ -14,11 +14,14 @@ constexpr int R1_Y = 90, R1_H = 80, R1_R = 15;
 constexpr int R2_Y = 180, R2_H = 50, R2_R = 10;
 constexpr int C1_X = MGN, C2_X = MGN + BTN_W + GAP;
 
-// --- Grille Options (3 colonnes x 2 lignes) ---
-// Largeur bouton option: (320 - 20 marge - 20 gap) / 3 = 93px
-constexpr int O_W = 93, O_H = 50, O_R = 8;
-constexpr int O_Y1 = 60, O_Y2 = 120;
-constexpr int O_X1 = 10, O_X2 = 113, O_X3 = 216;
+constexpr int O_W = BTN_W; 
+constexpr int O_H = 50;     
+constexpr int O_R = 8;
+constexpr int O_GAP_Y = 10; 
+
+constexpr int O_Y1 = 60;
+constexpr int O_Y2 = O_Y1 + O_H + O_GAP_Y; 
+constexpr int O_Y3 = O_Y2 + O_H + O_GAP_Y; 
 
 enum Screen {
     HOME,
@@ -28,24 +31,54 @@ enum Screen {
 Screen currentScreen = HOME;
 bool locked = false;
 
-// Boutons Accueil
-Button bLock(C1_X, R1_Y, BTN_W, R1_H, false, "L");
-Button bGreen(C2_X, R1_Y, BTN_W, R1_H, false, "G");
-Button bBlue(C1_X, R2_Y, BTN_W, R2_H, false, "B");
-Button bGrey(C2_X, R2_Y, BTN_W, R2_H, false, "GR");
+Button *bLock = nullptr;
+Button *bGreen = nullptr;
+Button *bBlue = nullptr;
+Button *bGrey = nullptr;
 
-// Boutons Options
-Button bOpt1(O_X1, O_Y1, O_W, O_H, false, "O1");
-Button bOpt2(O_X2, O_Y1, O_W, O_H, false, "O2");
-Button bOpt3(O_X3, O_Y1, O_W, O_H, false, "O3");
-Button bOpt4(O_X1, O_Y2, O_W, O_H, false, "O4");
-Button bOpt5(O_X2, O_Y2, O_W, O_H, false, "O5");
-Button bOpt6(O_X3, O_Y2, O_W, O_H, false, "O6");
+Button *bOpt1 = nullptr;
+Button *bOpt2 = nullptr;
+Button *bOpt3 = nullptr;
+Button *bOpt4 = nullptr;
+Button *bOpt5 = nullptr;
+Button *bOpt6 = nullptr; 
+
+void clearButtons() {
+    if (bLock) { delete bLock; bLock = nullptr; }
+    if (bGreen) { delete bGreen; bGreen = nullptr; }
+    if (bBlue) { delete bBlue; bBlue = nullptr; }
+    if (bGrey) { delete bGrey; bGrey = nullptr; }
+
+    if (bOpt1) { delete bOpt1; bOpt1 = nullptr; }
+    if (bOpt2) { delete bOpt2; bOpt2 = nullptr; }
+    if (bOpt3) { delete bOpt3; bOpt3 = nullptr; }
+    if (bOpt4) { delete bOpt4; bOpt4 = nullptr; }
+    if (bOpt5) { delete bOpt5; bOpt5 = nullptr; }
+    if (bOpt6) { delete bOpt6; bOpt6 = nullptr; }
+}
 
 void animateBtn(int x, int y, int w, int h, int r, uint16_t c) {
     M5.Lcd.drawRoundRect(x, y, w, h, r, C_WHITE);
     delay(100);
     M5.Lcd.drawRoundRect(x, y, w, h, r, c);
+}
+
+void initHomeButtons() {
+    clearButtons();
+    bLock = new Button(C1_X, R1_Y, BTN_W, R1_H, false, "L");
+    bGreen = new Button(C2_X, R1_Y, BTN_W, R1_H, false, "G");
+    bBlue = new Button(C1_X, R2_Y, BTN_W, R2_H, false, "B");
+    bGrey = new Button(C2_X, R2_Y, BTN_W, R2_H, false, "GR");
+}
+
+void initOptionsButtons() {
+    clearButtons();
+    bOpt1 = new Button(C1_X, O_Y1, O_W, O_H, false, "O1");
+    bOpt2 = new Button(C2_X, O_Y1, O_W, O_H, false, "O2");
+    bOpt3 = new Button(C1_X, O_Y2, O_W, O_H, false, "O3");
+    bOpt4 = new Button(C2_X, O_Y2, O_W, O_H, false, "O4");
+    bOpt5 = new Button(C1_X, O_Y3, O_W, O_H, false, "O5");
+    bOpt6 = new Button(C2_X, O_Y3, O_W, O_H, false, "O6");
 }
 
 void drawHome() {
@@ -56,15 +89,16 @@ void drawHome() {
     
     M5.Lcd.setTextSize(1);
     M5.Lcd.setFreeFont(&FreeSansBold18pt7b);
-    M5.Lcd.setTextDatum(ML_DATUM);
     
     String t1 = "Alarme Conne", t2 = "c", t3 = "t", t4 = "e", t5 = "e", t6 = "+";
     int startX = 1;
     
     if (locked) {
+        M5.Lcd.setTextDatum(MC_DATUM);
         M5.Lcd.setTextColor(C_WHITE, hBg);
-        M5.Lcd.drawString("Alarme Declenchee", startX, 24);
+        M5.Lcd.drawString("Alarme Active", W / 2, HEADER_H / 2);
     } else {
+        M5.Lcd.setTextDatum(ML_DATUM);
         M5.Lcd.setTextColor(C_WHITE, hBg);
         M5.Lcd.drawString(t1, startX, 24);
         M5.Lcd.setTextColor(C_RED1, hBg);
@@ -100,14 +134,12 @@ void drawHome() {
     M5.Lcd.setTextColor(C_WHITE, C_BLUE);
     M5.Lcd.setTextDatum(BC_DATUM);
     M5.Lcd.drawString("Options", (C1_X + BTN_W / 2) + 25, 218);
-    // Note: Assure-toi que 'gear' est bien dans images.h, sinon commente la ligne suivante
     M5.Lcd.pushImage((C1_X + (BTN_W - 50) / 2) - 43, R2_Y + (R2_H - 50) / 2, 50, 50, (uint16_t *)gear, 0x0000);
     
     M5.Lcd.fillRoundRect(C2_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
     M5.Lcd.setTextColor(C_WHITE, C_GREY);
     M5.Lcd.setTextDatum(BC_DATUM);
     M5.Lcd.drawString("Redemarrer", (C2_X + BTN_W / 2) + 25, 218);
-    // Note: Assure-toi que 'reboot_image' est bien dans images.h, sinon commente la ligne suivante
     M5.Lcd.pushImage((C2_X + (BTN_W - 50) / 2) - 43, R2_Y + (R2_H - 50) / 2, 50, 50, (uint16_t *)reboot_image, 0x0000);
 }
 
@@ -121,81 +153,82 @@ void drawOptions() {
     M5.Lcd.setTextColor(C_WHITE, C_BLUE);
     M5.Lcd.drawString("Reglages", 10, 28);
 
-    // Style de texte pour les boutons
     M5.Lcd.setFreeFont(&FreeSans12pt7b);
     M5.Lcd.setTextDatum(MC_DATUM);
     M5.Lcd.setTextColor(C_WHITE, C_BLUE);
 
-    // Ligne 1
-    M5.Lcd.fillRoundRect(C1_X, R1_Y, BTN_W, R1_H, R1_R, C_BLUE);
-    M5.Lcd.drawString("Wifi", O_X1 + BTN_W/2, O_Y1 + O_H/2);
+    M5.Lcd.fillRoundRect(C1_X, O_Y1, O_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Wifi", C1_X + O_W/2, O_Y1 + O_H/2);
 
-    M5.Lcd.fillRoundRect(C2_X, R1_Y, BTN_W, R1_H, R1_R, C_BLUE);
-    M5.Lcd.drawString("Son", O_X2 + BTN_W/2, O_Y1 + O_H/2);
+    M5.Lcd.fillRoundRect(C2_X, O_Y1, O_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Son", C2_X + O_W/2, O_Y1 + O_H/2);
 
-    M5.Lcd.fillRoundRect(O_X3, O_Y1, BTN_W, O_H, O_R, C_BLUE);
-    M5.Lcd.drawString("Ecran", O_X3 + BTN_W/2, O_Y1 + O_H/2);
+    M5.Lcd.fillRoundRect(C1_X, O_Y2, O_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Ecran", C1_X + O_W/2, O_Y2 + O_H/2);
 
-    // Ligne 2
-    M5.Lcd.fillRoundRect(O_X1, O_Y2, BTN_W, O_H, O_R, C_BLUE);
-    M5.Lcd.drawString("Journal", O_X1 + BTN_W/2, O_Y2 + O_H/2);
+    M5.Lcd.fillRoundRect(C2_X, O_Y2, O_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Journal", C2_X + O_W/2, O_Y2 + O_H/2);
 
-    M5.Lcd.fillRoundRect(O_X2, O_Y2, BTN_W, O_H, O_R, C_BLUE);
-    M5.Lcd.drawString("Capteur", O_X2 + BTN_W/2, O_Y2 + O_H/2);
+    M5.Lcd.fillRoundRect(C1_X, O_Y3, O_W, O_H, O_R, C_BLUE);
+    M5.Lcd.drawString("Capteurs", C1_X + O_W/2, O_Y3 + O_H/2);
 
-    // Bouton Retour
-    M5.Lcd.fillRoundRect(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
+    M5.Lcd.fillRoundRect(C2_X, O_Y3, O_W, O_H, O_R, C_GREY);
     M5.Lcd.setTextColor(C_WHITE, C_GREY);
-    M5.Lcd.drawString("Retour", C1_X + BTN_W / 2, R2_Y + R2_H / 2);
+    M5.Lcd.drawString("Retour", C2_X + O_W/2, O_Y3 + O_H/2);
 }
 
-void drawInterface() {
-    if (currentScreen == HOME) {
+void setScreen(Screen s) {
+    currentScreen = s;
+    if (s == HOME) {
+        initHomeButtons();
         drawHome();
     } else {
+        initOptionsButtons();
         drawOptions();
     }
 }
 
 void setup() {
     M5.begin();
-    drawInterface();
+    setScreen(HOME);
 }
 
 void loop() {
     M5.update();
     
     if (currentScreen == HOME) {
-        if (bLock.wasPressed()) { 
-            if (!locked) { locked = true; drawInterface(); }
+        if (bLock && bLock->wasPressed()) { 
+            if (!locked) { 
+                locked = true; 
+                drawHome(); 
+            }
             animateBtn(C1_X, R1_Y, BTN_W, R1_H, R1_R, C_RED);
         }
-        if (bGreen.wasPressed()) {
-            if (locked) { locked = false; drawInterface(); }
+        if (bGreen && bGreen->wasPressed()) {
+            if (locked) { 
+                locked = false; 
+                drawHome(); 
+            }
             animateBtn(C2_X, R1_Y, BTN_W, R1_H, R1_R, C_GREEN);
         }
-        if (bBlue.wasPressed()) {
+        if (bBlue && bBlue->wasPressed()) {
             animateBtn(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_BLUE);
-            currentScreen = OPTIONS;
-            drawInterface();
+            setScreen(OPTIONS);
         }
-        if (bGrey.wasPressed()) {
+        if (bGrey && bGrey->wasPressed()) {
              animateBtn(C2_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
         }
 
     } else if (currentScreen == OPTIONS) {
-        // Bouton Retour
-        if (bBlue.wasPressed()) {
-            animateBtn(C1_X, R2_Y, BTN_W, R2_H, R2_R, C_GREY);
-            currentScreen = HOME;
-            drawInterface();
+        if (bOpt1 && bOpt1->wasPressed()) animateBtn(C1_X, O_Y1, O_W, O_H, O_R, C_OPT);
+        if (bOpt2 && bOpt2->wasPressed()) animateBtn(C2_X, O_Y1, O_W, O_H, O_R, C_OPT);
+        if (bOpt3 && bOpt3->wasPressed()) animateBtn(C1_X, O_Y2, O_W, O_H, O_R, C_OPT);
+        if (bOpt4 && bOpt4->wasPressed()) animateBtn(C2_X, O_Y2, O_W, O_H, O_R, C_OPT);
+        if (bOpt5 && bOpt5->wasPressed()) animateBtn(C1_X, O_Y3, O_W, O_H, O_R, C_OPT);
+        
+        if (bOpt6 && bOpt6->wasPressed()) {
+            animateBtn(C2_X, O_Y3, O_W, O_H, O_R, C_GREY);
+            setScreen(HOME);
         }
-        // Gestion des 6 boutons (Animation simple pour l'instant)
-        if (bOpt1.wasPressed()) animateBtn(O_X1, O_Y1, O_W, O_H, O_R, C_OPT);
-        if (bOpt2.wasPressed()) animateBtn(O_X2, O_Y1, O_W, O_H, O_R, C_OPT);
-        if (bOpt3.wasPressed()) animateBtn(O_X3, O_Y1, O_W, O_H, O_R, C_OPT);
-        if (bOpt4.wasPressed()) animateBtn(O_X1, O_Y2, O_W, O_H, O_R, C_OPT);
-        if (bOpt5.wasPressed()) animateBtn(O_X2, O_Y2, O_W, O_H, O_R, C_OPT);
-        if (bOpt6.wasPressed()) animateBtn(O_X3, O_Y2, O_W, O_H, O_R, C_OPT);
     }
 }

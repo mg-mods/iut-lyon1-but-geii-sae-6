11b185ef5a4e203e23e5e2f1ce1158f3103a8f40
p2302682 <gaspard.denizou@etu.univ-lyon1.fr>
2026-05-07 15:43:25 +0200

    modification pour incorporer une image

---
+++ HF/test uart/test comm/blink.py
@@ -0,0 +1,24 @@
+from machine import Pin,UART
+from utime import sleep_ms
+
+pin = Pin("LED", Pin.OUT)
+
+
+def lectureReception ():
+    print(rec.read())
+
+
+print("LED starts flashing...")
+Tx = Pin(12)    
+comm = UART(0,9600)
+comm.init(9600,bits=8,parity=None,tx=Tx)
+rec = UART(1,9600)
+rec.init(9600,bits=8,parity=None,rx = Pin(9))
+
+while True:
+    comm.write(b'55')
+    rec.irq(lectureReception,rec.IRQ_RXIDLE)
+    sleep_ms(10)
+
+
+
